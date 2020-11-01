9e00bd337e33b2cbc08d9627875396b2bc4502cd
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-11-01 10:51:55 +0530

    graphql, express server starting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,64 @@
-console.log("Hello world!");
+import { ApolloServer } from "apollo-server-express";
+import connectRedis from "connect-redis";
+import express from "express";
+import session from "express-session";
+import Redis from "ioredis";
+import path from "path";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { COOKIE_NAME, __prod__ } from "./constants";
+import { HelloResolver } from "./resolvers/hello";
+
+const main = async (PORT: number) => {
+  const conn = await createConnection({
+    type: "postgres",
+    database: "swimmingly",
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    logging: true,
+    synchronize: true,
+    migrations: [path.join(__dirname, "./migrations/*")],
+    entities: [],
+  });
+  await conn.runMigrations();
+
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
+
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redis,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__,
+      },
+      saveUninitialized: false,
+      secret: "fijerifjerijeoijfeoijfoie",
+      resave: false,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is starting on localhost:${PORT}`);
+  });
+};
+
+main(4000).catch((err) => console.error(err));
