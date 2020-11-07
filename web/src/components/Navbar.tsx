e9be8e72f2144292d3e81b4a8584ee6a1c3d6a55
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-11-07 09:37:52 +0530

    navbar + login

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,35 +1,84 @@
-import { Box, Flex, Heading, Link, Text } from "@chakra-ui/core";
+import { useApolloClient } from "@apollo/client";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  useColorMode,
+} from "@chakra-ui/core";
 import NextLink from "next/link";
 import React from "react";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
 import { DarkModeSwitch } from "./DarkModeSwitch";
 
 interface NavbarProps {}
 
-export const Navbar: React.FC<NavbarProps> = () => {
+export const Navbar: React.FC<NavbarProps> = ({}) => {
+  const { colorMode } = useColorMode();
+  const [logout, { loading: logoutFetching }] = useLogoutMutation();
+  const apolloClient = useApolloClient();
+  const { data, loading } = useMeQuery({
+    skip: isServer(),
+  });
+
+  let body: JSX.Element;
+
+  // data is loading
+  if (loading) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex align="center">
+        <NextLink href="/create">
+          <Button as={Link} mr={4} color={colorMode === "light" && "teal.600"}>
+            create workout
+          </Button>
+        </NextLink>
+        <Box mr={2} color="white">
+          Logged in as {data.me.username}
+        </Box>
+        <Button
+          onClick={async () => {
+            await logout();
+            await apolloClient.resetStore();
+          }}
+          isLoading={logoutFetching}
+          variant="link"
+          color={colorMode === "dark" ? "white" : "teal.200"}
+        >
+          Logout
+        </Button>
+        <Box ml={2}>
+          <DarkModeSwitch />
+        </Box>
+      </Flex>
+    );
+  }
+
   return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding="1.5rem"
-      bg="teal.500"
-      color="white"
-    >
-      <Flex align="center" mr={5}>
+    <Flex zIndex={1} position="sticky" top={0} bg="teal.500" p={4}>
+      <Flex flex={1} m="auto" align="center">
         <NextLink href="/">
           <Link>
-            <Heading as="h1" size="xl" letterSpacing={"-.1.5rem"}>
-              Swimmingly
-            </Heading>
+            <Heading color="white">Swimmingly</Heading>
           </Link>
         </NextLink>
+        <Box ml={"auto"}>{body}</Box>
       </Flex>
-
-      <Box display={{ sm: "none", md: "block" }} mt={{ base: 4, md: 0 }}>
-        <Text>Logged in as ____</Text>
-        <DarkModeSwitch />
-      </Box>
     </Flex>
   );
 };
---
+++ web/src/pages/login.tsx
@@ -0,0 +1,101 @@
+import { Box, Button, Flex, Link } from "@chakra-ui/core";
+import { Form, Formik } from "formik";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import { Container } from "../components/Container";
+import { InputField } from "../components/InputField";
+import { Layout } from "../components/Layout";
+import { MeDocument, MeQuery, useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { withApollo } from "../utils/withApollo";
+
+const Login: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [login] = useLoginMutation();
+
+  return (
+    <Layout>
+      <Container variant="small">
+        <Formik
+          initialValues={{ username: "", email: "", password: "" }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await login({
+              variables: { options: values },
+              update: (cache, { data }) => {
+                cache.writeQuery<MeQuery>({
+                  query: MeDocument,
+                  data: {
+                    __typename: "Query",
+                    me: data?.login.user,
+                  },
+                });
+                cache.evict({ fieldName: "posts:{}" });
+              },
+            });
+            if (response.data?.login.errors) {
+              setErrors(toErrorMap(response.data.login.errors));
+            } else if (response.data?.login.user) {
+              if (typeof router.query.next === "string") {
+                router.push(router.query.next);
+              } else {
+                // if worked
+                router.push("/");
+              }
+            }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <InputField
+                name="username"
+                placeholder="username"
+                label="Username"
+              />
+
+              <Box mt={4}>
+                <InputField name="email" placeholder="email" label="Email" />
+              </Box>
+
+              <Box mt={4}>
+                <InputField
+                  name="password"
+                  placeholder="password"
+                  label="Password"
+                  type="password"
+                />
+                <Flex>
+                  <Box mt={2}>
+                    <NextLink href="/forgot-password">
+                      <Link ml="auto" style={{ color: "grey" }}>
+                        Forgot Password?
+                      </Link>
+                    </NextLink>
+                  </Box>
+
+                  <Box ml="auto" mt={2} position="unset">
+                    <NextLink href="/register">
+                      <Link ml="auto" style={{ color: "grey" }}>
+                        New to Swimmingly?
+                      </Link>
+                    </NextLink>
+                  </Box>
+                </Flex>
+              </Box>
+              <Button
+                mt={4}
+                type="submit"
+                variantColor="teal"
+                isLoading={isSubmitting}
+              >
+                Login
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </Layout>
+  );
+};
+
+export default withApollo({ ssr: false })(Login);
