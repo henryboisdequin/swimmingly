5e91878d85b24b987ed6c07bb9ec9ba8a516d805
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-11-07 09:50:00 +0530

    register

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,77 @@
+import { Box, Button } from "@chakra-ui/core";
+import { Form, Formik } from "formik";
+import { useRouter } from "next/router";
+import React from "react";
+import { Container } from "../components/Container";
+import { InputField } from "../components/InputField";
+import { Layout } from "../components/Layout";
+import { MeDocument, MeQuery, useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { withApollo } from "../utils/withApollo";
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
+  const [register] = useRegisterMutation();
+  return (
+    <Layout>
+      <Container variant="small">
+        <Formik
+          initialValues={{ username: "", email: "", password: "" }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await register({
+              variables: { options: values },
+              update: (cache, { data }) => {
+                cache.writeQuery<MeQuery>({
+                  query: MeDocument,
+                  data: {
+                    __typename: "Query",
+                    me: data?.register.user,
+                  },
+                });
+              },
+            });
+            if (response.data?.register.errors) {
+              setErrors(toErrorMap(response.data.register.errors));
+            } else if (response.data?.register.user) {
+              // if worked
+              router.push("/");
+            }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <InputField
+                name="username"
+                placeholder="username"
+                label="Username"
+              />
+              <Box mt={4}>
+                <InputField name="email" placeholder="email" label="Email" />
+              </Box>
+              <Box mt={4}>
+                <InputField
+                  name="password"
+                  placeholder="password"
+                  label="Password"
+                  type="password"
+                />
+              </Box>
+              <Button
+                mt={4}
+                type="submit"
+                isLoading={isSubmitting}
+                variantColor="teal"
+              >
+                register
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </Layout>
+  );
+};
+
+export default withApollo({ ssr: false })(Register);
